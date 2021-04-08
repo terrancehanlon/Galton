482dcaad21cda0bb8bbc82d2d5beb74d9cc15204
terrancehanlon <hanlo053@umn.edu>
2021-04-07 23:36:08 -0500

    engine eval function

--- chess-game.py
+++ chess-game.py
@@ -1,12 +1,16 @@
 from chess import Chess
 from printer import Printer
+from engine import Engine
 
 game = Chess()
 printer = Printer()
+engine = Engine()
 
 game.init_empty_board()
 game.init_pieces()
 game.move({'from': 'A2', 'to': 'A4'})
 printer.simple_print(game.board)
 printer.info_print(game.board)
+
+print(engine.simple_evaluate_board(game.board, game.w_pieces, game.b_pieces))
 
--- engine.py
+++ engine.py
@@ -5,8 +5,28 @@
 
 class Engine:
 
-    def evaluate(self, board):
-        print()
+    def evaluate_pieces(self, pieces1, pieces2):
+        return 200 * (pieces1['K'] - pieces2['K']) + 9 * (pieces1['Q'] - pieces2['Q']) + 5 * (pieces1['R'] - pieces2['R']) + 3 * (pieces1['B'] - pieces2['B']) + 1 * (pieces1['P'] - pieces2['P'])
+
+    def simple_evaluate_board(self, board, w_pieces, b_pieces):
+        w_eval_dict = {}
+        b_eval_dict = {}
+
+        for w in w_pieces:
+            if w['type'] in w_eval_dict:
+                w_eval_dict[w['type']] = w_eval_dict[w['type']] + 1
+            else:
+                w_eval_dict[w['type']] = 1
+            
+        
+        for b in b_pieces:
+            if b['type'] in b_eval_dict:
+                b_eval_dict[b['type']] = b_eval_dict[b['type']] + 1
+            else:
+                b_eval_dict[b['type']] = 1
+
+        return {'white_score': 1 * self.evaluate_pieces(w_eval_dict, b_eval_dict), 'black_score': -1 * self.evaluate_pieces(w_eval_dict, b_eval_dict)}
+        
 
     def minimax(self, board):
         print()
