bf2d0a035cedf012a4f6f35699640bb6b86f919d
terrancehanlon <hanlo053@umn.edu>
2021-04-10 23:49:28 -0500

    add ranks and files to simple print

--- printer.py
+++ printer.py
@@ -27,15 +27,17 @@ class Printer:
         start_letter = 'A'
         start_number = 8
         tracker = 1
-
+        side_tracker = 8
+        
         for i in range(1, 65):
             if tracker > 8:
+                print(" ", str(side_tracker))
                 tracker = 1
                 start_letter = 'A'
                 start_number = start_number - 1
-                print()
+                side_tracker = side_tracker - 1
             if board[start_letter + str(start_number)] != 0:
-                if board[start_letter + str(start_number)]['is_player']:\
+                if board[start_letter + str(start_number)]['is_player']:
                     print(colored(board[start_letter + str(start_number)]['type'], 'yellow'), " ", end='')
                 else:
                     print(colored(board[start_letter + str(start_number)]['type'], 'red'), " ", end='')
@@ -43,8 +45,13 @@ class Printer:
                 print(board[start_letter + str(start_number)], " ", end='')
             start_letter = chr(ord(start_letter) + 1)
             tracker = tracker + 1
+        print(" ", "1")
         print()
-
+        start_letter = 'A'
+        for i in range (1,9):
+            print(start_letter, " ", end='')
+            start_letter = chr(ord(start_letter) + 1)
+        print()
     def w_pieces_print(self, w_pieces):
         for p in w_pieces:
             print(p)
